bf0295d694e875987666fb3a571a60a6b483133b
Your Name <you@example.com>
2018-07-04 13:41:09 +0300

    add functionality to application

--- zabbix-angular/src/app/app.component.ts
+++ zabbix-angular/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  ngOnInit(){
+    $(document).ready(function(){
+      // alert('test')
+    });
+  }
+
   title = 'app';
 }
--- zabbix-angular/src/app/app.module.ts
+++ zabbix-angular/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { BodyComponent } from './body/body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule
