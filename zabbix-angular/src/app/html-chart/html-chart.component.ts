a0b80b9a99a0f209e4dae42ee4dc30d23289acfc
Your Name <you@example.com>
2018-07-25 09:41:48 +0300

    test charts 2

--- zabbix-angular/src/app/html-chart/html-chart.component.ts
+++ zabbix-angular/src/app/html-chart/html-chart.component.ts
@@ -1,8 +1,8 @@
+import { GenDataService } from './../gen-data.service';
 import { Component, OnInit, AfterContentInit, AfterViewInit,ViewChildren } from '@angular/core';
 import { Chart } from 'chart.js';
 import { ZFunctionsService } from './../z-functions.service';
 import { SorterService } from './../sorter.service';
-import { GenDataService } from '../gen-data.service';
 import { getLocaleMonthNames } from '../../../node_modules/@angular/common';
 
 
@@ -28,7 +28,7 @@ export class HtmlChartComponent implements OnInit {
     year;
     collapse = false;
 
-    chartsArr = [0,0];
+    // chartsArr = [0,0];
     charts = [];
     chart = [];
 
@@ -38,16 +38,7 @@ export class HtmlChartComponent implements OnInit {
     ranges = [30,90,180];
     selectedRange;
 
-    arrLocal = {
-      "isLapos"  : {
-        "v"  : 0,
-        "p"  : 0 
-      },
-      "nonLapos"  : {
-        "v"  : 0,
-        "p"  : 0 
-      },
-    };
+    arrLocal = {};
 
     constructor(private service:GenDataService, private sorter:SorterService,private zFunctions:ZFunctionsService){
 
@@ -58,6 +49,7 @@ export class HtmlChartComponent implements OnInit {
           var that = this;
           this.year = this.service.year;
           this.selectedRange = this.ranges[0];
+          this.addDatatoObject();
           
           this.chartOptions = {
             legend: {
@@ -84,53 +76,87 @@ export class HtmlChartComponent implements OnInit {
 
           this.service.observeSum.subscribe(message => {
               this.sum = getParams(message);
-              debugger;
-              this.initLaposObj();
+
+              // this.initLaposObj();
+
               function getParams(message){
                 
-                that.selectedRegion = (that.selectedRegion === '') ? 'AMER' : that.selectedRegion;
-              
+                
+                  var regions = that.service.regionArr;
 
-                for (let k of that.zFunctions.objectKeys(message[that.selectedRegion])) {
-              
-                    that.chartData['labels'].push(k);
+                  for (let index = 0; index < regions.length; index++) { //collect data for  on allregions
 
-                    var laposObj = message[that.selectedRegion][k].Lapos.dates;
+                        const region = regions[index];
 
-                    for (var key in laposObj) {
-                      if (laposObj.hasOwnProperty(key)) {
-                          // console.log(key , laposObj[key]);
-                          var valid = that.validDate(key);
+                        for (let k of that.zFunctions.objectKeys(message[region])) {
+                    
+                          that.chartData['labels'].push(k);
 
-                          if(valid){
-                            var currentobject = laposObj[key];
+                          var laposObj = message[region][k].Lapos.dates;
 
-                            for (var isKey in currentobject) {
-                              if (currentobject.hasOwnProperty(isKey)) {
-                                // console.log(isKey , currentobject[isKey]);
-                                var secondObj = currentobject[isKey];
+                          for (var key in laposObj) {
+                            if (laposObj.hasOwnProperty(key)) {
+                                // console.log(key , laposObj[key]);
+                                var valid = that.validDate(key);
 
-                                for (var vKey in secondObj) {
-                                  if (secondObj.hasOwnProperty(vKey)) {
-                                    // console.log(vKey , secondObj[vKey]);
-                                    that.arrLocal[isKey][vKey] += secondObj[vKey];
+                                if(valid){
+                                  var currentobject = laposObj[key];
+
+                                  for (var isKey in currentobject) {
+                                    if (currentobject.hasOwnProperty(isKey)) {
+                                      // console.log(isKey , currentobject[isKey]);
+                                      var secondObj = currentobject[isKey];
+
+                                      for (var vKey in secondObj) {
+                                        if (secondObj.hasOwnProperty(vKey)) {
+                                          // console.log(vKey , secondObj[vKey]);
+
+                                            
+                                          // that.arrLocal[region] = that.arrLocal[region] || {};
+                                          // that.arrLocal[region][isKey] = that.arrLocal[region][isKey] || {};
+                                          // that.arrLocal[region][isKey][vKey] = that.arrLocal[region][isKey][vKey] || 0;
+
+                                          that.arrLocal[region][isKey][vKey] += secondObj[vKey];
+                                        }
+                                      }
+                                    }
                                   }
                                 }
-                              }
                             }
                           }
+
                       }
-                    }
+                  }
 
-                }
 
-                console.log(that.arrLocal);
+                  console.log(that.arrLocal);
               }
           });
 
-          for (let i = 0; i < this.chartsArr.length; i++) {
+          for (let i = 0; i < 2; i++) {
             this.charts.push(i);
           }
+    }
+
+    addDatatoObject(){
+      var regions = this.service.regionArr;
+
+      for (let index = 0; index < regions.length; index++) {
+        const region = regions[index];
+
+        this.arrLocal[region] = {
+          "isLapos"  : {
+            "v"  : 0,
+            "p"  : 0 
+          },
+          "nonLapos"  : {
+            "v"  : 0,
+            "p"  : 0 
+          },
+        };
+        
+      }
+
     }
 
     initLaposObj(){
@@ -142,14 +168,14 @@ export class HtmlChartComponent implements OnInit {
           backgroundColor: 'rgba(255, 99, 132, 0.2)',
           borderColor: 'rgba(255,99,132,1)',
           borderWidth: 1,
-          data: this.arrLocal['isLapos']['p'],
+          data: [this.arrLocal['isLapos']['p']],
       }
       var Lapos_v = {
           label: "Lapos virtual",
           backgroundColor: 'rgba(54, 162, 235, 0.2)',
           borderColor:  'rgba(54, 162, 235, 1)',
           borderWidth: 1,
-          data: this.arrLocal['isLapos']['v'],
+          data: [this.arrLocal['isLapos']['v']],
       }
 
       var non_Lapos_p = {
@@ -157,7 +183,7 @@ export class HtmlChartComponent implements OnInit {
           backgroundColor: 'rgba(255, 206, 86, 0.2)',
           borderColor: 'rgba(255, 206, 86, 1)',
           borderWidth: 1,
-          data: this.arrLocal['nonLapos']['p'],
+          data: [this.arrLocal['nonLapos']['p']],
       }
 
       var non_Lapos_v = {
@@ -165,34 +191,34 @@ export class HtmlChartComponent implements OnInit {
           backgroundColor: 'rgba(75, 192, 192, 0.2)',
           borderColor:   'rgba(75, 192, 192, 1)',
           borderWidth: 1,
-          data: this.arrLocal['nonLapos']['v'],
+          data: [this.arrLocal['nonLapos']['v']],
       }
 
       this.chartData['datasets'].push(Lapos_p,Lapos_v,non_Lapos_p,non_Lapos_v);
     
     }
 
-  ngAfterViewInit() {
-      for (let i = 0; i < this.chartsArr.length; i++) {
-          var canvas = 'canvas' + i.toString();
-          var inChart = new Chart(canvas, {
-            type: 'bar',
-            data: this.chartData,
-            options: this.chartOptions 
-          });
-          this.charts.push(inChart);
-      }
-  }
+    ngAfterViewInit() {
+        for (let i = 0; i < 2; i++) {
+            var canvas = 'canvas' + i.toString();
+            var inChart = new Chart(canvas, {
+              type: 'bar',
+              data: this.chartData,
+              options: this.chartOptions 
+            });
+            this.charts.push(inChart);
+        }
+    }
 
-  validDate(myDate){
+    validDate(myDate){
 
-      myDate=myDate.split("-");
-      var newDate=myDate[1]+"/"+myDate[2]+"/"+myDate[0];
-      var today = new Date().getTime();
-      var end = new Date(newDate).getTime();
-      var range = this.selectedRange * 24 * 60 * 60 * 1000; 
-      var valid = (today - end) < range;
+        myDate=myDate.split("-");
+        var newDate=myDate[1]+"/"+myDate[2]+"/"+myDate[0];
+        var today = new Date().getTime();
+        var end = new Date(newDate).getTime();
+        var range = this.selectedRange * 24 * 60 * 60 * 1000; 
+        var valid = (today - end) < range;
 
-      return valid;
-  }
+        return valid;
+    }
 }
